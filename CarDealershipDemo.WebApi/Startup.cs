ac746e876c962cd17f43db7528ba76e2e4d185bc
Matthew Davidsohn <matthew.davidsohn@outlook.com>
2021-09-05 16:21:31 -0400

    Revert startup changes for spa static files

--- CarDealershipDemo.WebApi/Startup.cs
+++ CarDealershipDemo.WebApi/Startup.cs
@@ -37,7 +37,7 @@ namespace CarDealershipDemo.WebApi
             services.AddScoped<ICarsService, CarsService>();
 
             services.AddSpaStaticFiles(config => {
-                config.RootPath = "ClientApp/build";
+                config.RootPath = "app";
             });
         }
 
@@ -66,7 +66,7 @@ namespace CarDealershipDemo.WebApi
 
             app.UseSpa(spa => 
             {
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "/";
             });
         }
     }
