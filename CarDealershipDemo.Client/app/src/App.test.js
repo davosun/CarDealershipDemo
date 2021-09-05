579ff99002d183ec14c6c40e81cf6eea09d5760a
Matthew Davidsohn <matthew.davidsohn@outlook.com>
2021-09-05 16:14:24 -0400

    Remove client directory from api project. Re-add npm build of client for deployment.

--- CarDealershipDemo.Client/app/src/App.test.js
+++ CarDealershipDemo.Client/app/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders car dealership header', () => {
   render(<App />);
   const linkElement = screen.getByText(/car dealership/i);
   expect(linkElement).toBeInTheDocument();
